73e11f7dcee8553fd2d3922ea5b15826d4430164
Jedeo <jedeopastor@gmail.com>
2022-11-15 13:11:47 -0700

    Add: class names

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import './UrlContainer.css';
+import React from "react";
+import "./UrlContainer.css";
+import { deletePost } from "../../apiCalls";
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+const UrlContainer = (props) => {
+  const urlEls = props.urls.map((url) => {
     return (
       <div key={url.id} className="url">
         <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
+        <a href={url.short_url} target="blank">
+          {url.short_url}
+        </a>
         <p>{url.long_url}</p>
+
+        <span className="delete" onClick={() => deletePost(url.id)}>&#10005;</span>
       </div>
-    )
+    );
   });
 
   return (
-    <section className='card-container'>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
+    <section className="card-container">
+      {urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p>}
     </section>
-  )
-}
+  );
+};
 
 export default UrlContainer;
